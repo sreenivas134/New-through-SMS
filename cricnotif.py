4a177cdbbed365cb59d44bd317e15e7f823a9a7a
sreenivas pallapu <sreenivas.engece@gmail.com>
2015-03-29 09:40:42 +0530

    first commit

---
+++ cricnotif.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+
+import requests
+from bs4 import BeautifulSoup
+import pynotify
+from time import sleep
+from sendsms import sendsms
+"""def sendmessage(title, message):
+    pynotify.init("Test")
+    notice = pynotify.Notification(title, message)
+    notice.show()
+    return"""
+
+url = "https://news.google.co.in/"
+number = raw_input("Enter Mobile number: ")
+while True:
+    r = requests.get(url)
+    while r.status_code is not 200:
+            r = requests.get(url)
+
+    soup = BeautifulSoup(r.text)
+    data = soup.find_all("div", class_='esc-lead-snippet-wrapper')
+    for head in data[5:10]:
+    	score = head.text[:128]
+    	score=score+'...\n -sreenu'
+    	#sendmessage('News', score)
+    	sendsms('8142936858','bindu', score, number)
+    	sleep(8)
